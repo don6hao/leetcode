cb3d78c6d65376d48c33d0be720d769ed9bf70a1
don6hao <don6hao@gmail.com>
2014-12-19 10:57:50 +0800

    length of last word

---
+++ LengthOfLastWord.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def lengthOfLastWord(self, s):
+        if 0x00 == len(s):
+            return 0x00
+        length = 0x00
+        flag   = False
+        for c in s[::-1]:
+            if ' ' == c:
+                if True == flag:
+                    break
+                continue
+            length += 1
+            flag = True
+        return length
+
+if __name__ == '__main__':
+    p = Solution()
+    print p.lengthOfLastWord('11 111   ')
+    print p.lengthOfLastWord('11 ')
